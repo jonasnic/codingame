30a3ac6dde4fd61c5f5f059b542266e09b2038c1
Charles-Antoine Francisco <charlantfr@gmail.com>
2019-04-04 15:58:40 -0400

    Update README

--- README.md
+++ README.md
@@ -1,44 +1,42 @@
 # Solutions to Codingame Puzzles
 
-[CodinGame Profile](https://www.codingame.com/profile/8111ec5700e5b6591daabfc46fd79e278747932)
+This repository contains solutions to original puzzles, community puzzles, contests and Clash of Code.
 
-## Table of Contents
+## Puzzles
 
 ### Easy Puzzles
-* [ASCII Art](selected-puzzles/easy/ascii-art)
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
+* [ASCII Art in Python](selected-puzzles/easy/ascii-art)
+* [Chuck Norris in Haskell](selected-puzzles/easy/chuck-norris)
+* [Defibrillators in Kotlin](selected-puzzles/easy/defibrillators)
+* [Horse-racing Duals in Ruby](selected-puzzles/easy/horse-racing-duals)
+* [Mars Lander - Episode 1 in JavaScript](selected-puzzles/easy/mars-lander1)
+* [MIME Type in Python](selected-puzzles/easy/mime-type)
+* [Power of Thor - Episode 1 in Python](selected-puzzles/easy/power-of-thor1)
+* [Temperatures in Python](selected-puzzles/easy/temperatures)
+* [The Descent in Kotlin](selected-puzzles/easy/the-descent)
 
 ### Medium Puzzles
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
+* [Bender - Episode 1 in Python](selected-puzzles/medium/bender1)
+* [Conway Sequence in Haskell](selected-puzzles/medium/)
+* [The Last Crusade - Episode 1 in Python](selected-puzzles/medium/last-crusade)
+* [Mayan Calculation in Kotlin](selected-puzzles/medium/mayan-calculation)
+* [Scrabble in Python](selected-puzzles/medium/srabble)
+* [Shadows of the Knight - Episode 1 in Ruby](selected-puzzles/medium/shadows-knight1)
+* [Skynet Revolution - Episode 1 in Java](selected-puzzles/medium/skynet-revolution1)
+* [There is no Spoon - Episode 1 in Python](selected-puzzles/medium/there-is-no-spoon1)
+* [War in Kotlin](selected-puzzles/medium/war)
 
 ### Hard Puzzles
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
-* []()
+* [Bender - Episode 2 in Python](selected-puzzles/hard/bender2)
+* [Roller Coaster in Python](selected-puzzles/hard/roller-coaster)
+* [Skynet Revolution - Episode 2 in Python](selected-puzzles/hard)
+* [Super Computer in Python](selected-puzzles/hard/super-computer)
+* [Surface in Python](selected-puzzles/hard/surface)
+* [TAN Network in Python](selected-puzzles/hard/tan-network)
+* [The Labyrinth in Python](selected-puzzles/hard/the-labyrinth)
+* [Vox Codei - Episode 1 in Python](selected-puzzles/hard/vox-codei1)
+
+[CodinGame Profile](https://www.codingame.com/profile/8111ec5700e5b6591daabfc46fd79e278747932)
 
 ## Other CodinGamers
 * [ethiery](https://ethiery.github.io/codingame/index.html)
--- selected-puzzles/hard/tan-network/tan_network.py
+++ selected-puzzles/hard/tan-network/tan_network.py
@@ -1,119 +1,134 @@
 import math
 
+from collections import defaultdict
+
 
 class Node:
-    def __init__(self, identifier, full_name, lat, lon):
-        self.routes = []
-        self.identifier = identifier
-        self.full_name = full_name
+    def __init__(self, name, lat, lon):
+        self.name = name
         self.lat = lat  # latitude
         self.lon = lon  # longitude
-
-    def distance(self, other):
-        x = (other.lon - self.lon) * math.cos((self.lat + other.lat) / 2)
-        y = other.lat - self.lat
-        return math.hypot(x, y) * 6371
+        self.routes = []
 
 
 class Graph:
-    def __init__(self):
-        self.nodes = {}  # identifier, node
+    def __init__(self, nodes, start_id, end_id):
+        self.nodes = nodes  # node id => node object
+        self.start_id = start_id
+        self.end_id = end_id
 
-    def read_game_input(self):
-        self.start_id = input()  # start id
-        self.end_id = input()  # end id
-        nb_stops = int(input())
-        for _ in range(nb_stops):
-            line = input().split(",")
-            identifier = line[0]
-            full_name = line[1].replace("\"", "", 2)
-            lat = math.radians(float(line[3]))
-            lon = math.radians(float(line[4]))
-            node = Node(identifier, full_name, lat, lon)
-            self.nodes[identifier] = node
+    @property
+    def start_node(self):
+        return self.nodes[self.start_id]
 
-        nb_routes = int(input())
-        for _ in range(nb_routes):
-            route = input().split(" ")
-            identifier1 = route[0]
-            identifier2 = route[1]
-            self.nodes[identifier1].routes.append(identifier2)
+    @property
+    def end_node(self):
+        return self.nodes[self.end_id]
 
-    def search(self):
-        """Compute the shortest path between start and end using A*
-        Reference: https://en.wikipedia.org/wiki/A*_search_algorithm
-        """
-        closed = set()  # set of ids already evaluated
-        opened = set()  # set of discovered ids that are not evaluated yet
-        parents = {}  # identifier: parent_id
-        g_scores = {}  # identifier: g_score
-        f_scores = {}  # identifier: f_score
-        opened.add(self.start_id)
-        g_scores[self.start_id] = 0
-        f_scores[self.start_id] = self.h_cost(self.nodes[self.start_id])
 
-        while len(opened) != 0:
-            min_id = self.minimum(opened, f_scores)
-            if min_id is None:
-                min_id = opened.pop()
-            else:
-                opened.remove(min_id)
-            if min_id == self.end_id:
-                return parents  # search is done
-            closed.add(min_id)
-            self.explore(
-                min_id, closed, opened, parents, g_scores, f_scores
-            )
+def distance(nodeA, nodeB):
+    EARTH_RADIUS = 6371
+    x = (nodeB.lon - nodeA.lon) * math.cos((nodeA.lat + nodeB.lat) / 2)
+    y = nodeB.lat - nodeA.lat
+    return math.hypot(x, y) * EARTH_RADIUS
 
-    def h_cost(self, current_node):
-        return current_node.distance(self.nodes[self.end_id])
 
-    def minimum(self, opened, f_scores):
-        min_id = None
-        min_f_score = math.inf
-        for identifier in opened:
-            f_score = f_scores.get(identifier, math.inf)
-            if (f_score < min_f_score):
-                min_id = identifier
-                min_f_score = f_score
-        return min_id
+def h_cost(nodeA, nodeB):
+    return distance(nodeA, nodeB)
 
-    def explore(self, min_id, closed, opened, parents, g_scores, f_scores):
-        min_node = self.nodes[min_id]
-        for neighbor_id in min_node.routes:
-            if neighbor_id not in closed:
-                if neighbor_id not in opened:
-                    opened.add(neighbor_id)
-                node = self.nodes[neighbor_id]
-                g_score = g_scores.get(min_id, math.inf)
-                g_score += min_node.distance(node)
-                if g_score < g_scores.get(neighbor_id, math.inf):
-                    parents[neighbor_id] = min_id
-                    g_scores[neighbor_id] = g_score
-                    h_score = self.h_cost(node)
-                    f_scores[neighbor_id] = g_score + h_score
 
-    def solve(self):
-        if self.start_id == self.end_id:
-            print(self.nodes[self.start_id].full_name)
-            exit()
+def read_game_input():
+    nodes = {}
+    start_id = input()
+    end_id = input()
 
-        parents = self.search()
-        current_id = self.end_id
-        stack = []
+    n = int(input())
+    for i in range(n):
+        stop = input().split(',')
+        identifier = stop[0]
+        name = stop[1].replace('\"', '')
+        lat = math.radians(float(stop[3]))
+        lon = math.radians(float(stop[4]))
+        node = Node(name, lat, lon)
+        nodes[identifier] = node
 
-        if parents is None:
-            print("IMPOSSIBLE")
-            exit()
+    m = int(input())
+    for i in range(m):
+        idA, idB = input().split(' ')
+        nodes[idA].routes.append(idB)
 
-        while current_id in parents:
-            stack.append(current_id)
-            current_id = parents[current_id]
+    return Graph(nodes, start_id, end_id)
+
+
+def a_star(graph):
+    """Compute the shortest path between start_id and end_id with A*"""
+    # initialization
+    closed_set = set()  # set of node ids already evaluated
+    open_set = {graph.start_id}  # set of node ids to evaluate
+    came_from = {}  # node_id => parent node id on the shortest path
+    g_scores = defaultdict(lambda: math.inf)  # node_id => cost from start_node
+    f_scores = defaultdict(lambda: math.inf)  # node_id => g_score + h_cost to end_node
+    g_scores[graph.start_id] = 0
+    f_scores[graph.start_id] = h_cost(graph.start_node, graph.end_node)
+
+    # loop until end_node is found or we run out of nodes to evaluate
+    while len(open_set) != 0:
+        current_id = minimum(open_set, f_scores)
+        if current_id == graph.end_id:
+            return came_from
+        # mark current as visited
+        open_set.remove(current_id)
+        closed_set.add(current_id)
+        
+        # explore neighbors
+        for neighbor_id in graph.nodes[current_id].routes:
+            if neighbor_id in closed_set:
+                continue  # already evalutated this node
+            neighbor_node = graph.nodes[neighbor_id]
+            g_score = g_scores[current_id] + distance(graph.nodes[current_id], neighbor_node)
+            if neighbor_id not in open_set:
+                open_set.add(neighbor_id)  # first time we explore this node id
+            elif g_score >= g_scores[neighbor_id]:
+                continue  # we already explored this node and the new cost is more expensive
+            came_from[neighbor_id] = current_id
+            g_scores[neighbor_id] = g_score
+            f_scores[neighbor_id] = g_score + h_cost(neighbor_node, graph.end_node)
+    return None
+
+
+def minimum(open_set, f_scores):
+    """Compute the node_id in open_set with the minimum f_score"""
+    min_id = None
+    min_f_score = math.inf
+    for identifier in open_set:
+        f_score = f_scores[identifier]
+        if f_score < min_f_score:
+            min_id = identifier
+            min_f_score = f_score
+    return min_id
+
+
+def reconstruct_path(graph, came_from):
+    current_id = graph.end_id
+    stack = []
+
+    while current_id in came_from:
         stack.append(current_id)
-        while len(stack) != 0:
-            print(self.nodes[stack.pop()].full_name)
+        current_id = came_from[current_id]
+    stack.append(current_id)
+    return stack
 
 
-graph = Graph()
-graph.read_game_input()
-graph.solve()
+def print_solution(graph, came_from):
+    if came_from is None:
+        print("IMPOSSIBLE")
+    else:
+        path = reconstruct_path(graph, came_from)
+        while len(path) != 0:
+            node = graph.nodes[path.pop()]
+            print(node.name)
+
+
+graph = read_game_input()
+came_from = a_star(graph)
+print_solution(graph, came_from)
