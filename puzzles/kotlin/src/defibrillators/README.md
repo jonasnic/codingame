d4f7364c9738349094650219e2e81d55888fdd87
Charles-Antoine Francisco <charlantfr@gmail.com>
2019-04-08 14:58:25 -0400

    Last 4 Easy Puzzles in TS

--- puzzles/kotlin/src/defibrillators/README.md
+++ puzzles/kotlin/src/defibrillators/README.md
@@ -1,6 +1,5 @@
 # Defibrillators
 
-This is my favourite puzzle from the Easy section.
-The idea is to find the defibrillator closest to the user's position.
+The goal of this puzzle is to find the defibrillator closest to the user's position.
 I suggest you to use the formula provided in the problem statement.
 The calculations have to be exact in order to find the correct defibrillator.
---
+++ puzzles/ts/mime-type/mime-type.ts
@@ -0,0 +1,23 @@
+const nbElements = Number(readline()); // Number of elements which make up the association table.
+const nbNames = Number(readline()); // Number of file names to be analyzed.
+let table: object = {} // object to map file extensions to mime types
+for (let i: number = 0; i < nbElements; i++) {
+  const [extension, mimeType]: [string, string] = readline().split(' ');
+  table[extension.toLowerCase()] = mimeType;
+}
+
+// For each of the filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
+for (let i: number = 0; i < nbNames; i++) {
+  const name: string = readline();
+  const dotIndex: number = name.lastIndexOf('.');
+  if (dotIndex === -1 || dotIndex === name.length - 1) {
+    console.log("UNKNOWN");
+  } else {
+    const extension: string = name.substring(dotIndex + 1).toLowerCase();
+    if (extension in table) {
+      console.log(table[extension]);
+    } else {
+      console.log("UNKNOWN");
+    }
+  }
+}
