e5dca48252eb334b2a358e7341278ef5d1a43abc
Charles-Antoine Francisco <charlantfr@gmail.com>
2019-04-22 09:06:47 -0400

    super_computer.py

--- puzzles/python3/super-computer/super_computer.py
+++ puzzles/python3/super-computer/super_computer.py
@@ -1,20 +1,21 @@
+# read game input
 tasks = []
-n = int(input())
-for i in range(n):
-    start_day, duration = [int(j) for j in input().split()]
-    task = (start_day + duration - 1, start_day)  # end day, start day
-    tasks.append(task)
+nb_tasks = int(input())
+for i in range(nb_tasks):
+    starting_day, duration = map(int, input().split())
+    ending_day = starting_day + duration - 1
+    tasks.append((starting_day, ending_day))
 
-tasks.sort()
+# Greedy Iterative Activity Selector
+tasks.sort(key=lambda task: task[1])
+selected_tasks = {tasks[0]}
+previous_task_index = 0
+for i in range(1, nb_tasks):
+    _, previous_end_day = tasks[previous_task_index]
+    starting_day, _ = tasks[i]
+    # check if task doesn't overlap
+    if starting_day > previous_end_day:
+        selected_tasks.add(tasks[i])
+        previous_task_index = i
 
-carry_list = []
-carry_list.append(tasks[0])
-prev_index = 0
-for i in range(1, n):
-    task = tasks[i]
-    prev_task = tasks[prev_index]
-    if task[1] > prev_task[0]:  # check if task doesn't overlap
-        carry_list.append(task)
-        prev_index = i
-
-print(len(carry_list))
+print(len(selected_tasks))
