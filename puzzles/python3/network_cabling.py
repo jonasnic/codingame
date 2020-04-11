e5a2ca51705e911a8f424ba8375b357f3d54b9f3
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-04-11 10:22:52 -0400

    Network Cabling - clean up

--- puzzles/python3/network_cabling.py
+++ puzzles/python3/network_cabling.py
@@ -1,12 +1,8 @@
-import math
-
-
 if __name__ == "__main__":
     nb_buildings = int(input())
-    cable_length = 0
+    y_coordinates = []
     min_x = float("inf")
     max_x = 0
-    y_coordinates = []
 
     for _ in range(nb_buildings):
         x, y = map(int, input().split())
@@ -17,14 +13,14 @@ if __name__ == "__main__":
             max_x = x
 
     # length of horizontal cable
-    cable_length += (max_x - min_x)
+    cable_length = max_x - min_x
     
     # calculate y median to minimize the length of vertical cable(s)
     y_coordinates.sort()
     y_median = y_coordinates[nb_buildings // 2]
-    
+
     # length of vertical cable(s)
     for y in y_coordinates:
-        cable_length += math.fabs(y_median - y)
-    
-    print(int(cable_length))
+        cable_length += abs(y_median - y)
+
+    print(cable_length)
