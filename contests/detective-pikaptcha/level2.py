541a88f707e40d048060305157df8b4e1d91edff
Charles-Antoine Francisco <charlantfr@gmail.com>
2020-04-28 09:53:17 -0400

    Detective Pikaptcha - Level 2 - Review

--- contests/detective-pikaptcha/level2.py
+++ contests/detective-pikaptcha/level2.py
@@ -43,7 +43,7 @@ class Game:
         self.rows = []
         self.width = 0
         self.height = 0
-        self.pikachu = Pikachu()
+        self.pika = Pikachu()
 
     def read_input(self):
         self.width, self.height = map(int, input().split())
@@ -52,70 +52,68 @@ class Game:
             self.rows.append([])
             for x in range(self.width):
                 if line[x] in DIRECTIONS:
-                    self.pikachu.start = (x, y)
-                    self.pikachu.x = x
-                    self.pikachu.y = y
-                    self.pikachu.facing = line[x]
+                    self.pika.start = (x, y)
+                    self.pika.x = x
+                    self.pika.y = y
+                    self.pika.facing = line[x]
                 if line[x] == WALL:
                     self.rows[y].append(WALL)
                 else:
                     self.rows[y].append(0)
         follow = input()
         if follow == FOLLOW_RIGHT:
-            self.pikachu.priorities = FOLLOW_RIGHT_PRIORITIES
+            self.pika.priorities = FOLLOW_RIGHT_PRIORITIES
         elif follow == FOLLOW_LEFT:
-            self.pikachu.priorities = FOLLOW_LEFT_PRIORITIES
+            self.pika.priorities = FOLLOW_LEFT_PRIORITIES
 
     def solve(self):
         while not self.is_pikachu_back_at_the_start():
-            priority_list = self.pikachu.priorities[self.pikachu.facing]
-            self.pikachu.previous = (self.pikachu.x, self.pikachu.y)
+            priority_list = self.pika.priorities[self.pika.facing]
+            self.pika.previous = (self.pika.x, self.pika.y)
 
             for priority in priority_list:
                 if priority == RIGHT:
                     if self.pikachu_can_go_right():
-                        self.pikachu.facing = RIGHT
-                        self.pikachu.x += 1
+                        self.pika.facing = RIGHT
+                        self.pika.x += 1
                         break
                 elif priority == DOWN:
                     if self.pikachu_can_go_down():
-                        self.pikachu.facing = DOWN
-                        self.pikachu.y += 1
+                        self.pika.facing = DOWN
+                        self.pika.y += 1
                         break
                 elif priority == LEFT:
                     if self.pikachu_can_go_left():
-                        self.pikachu.facing = LEFT
-                        self.pikachu.x -= 1
+                        self.pika.facing = LEFT
+                        self.pika.x -= 1
                         break
-                else:
+                elif priority == UP:
                     if self.pikachu_can_go_up():
-                        self.pikachu.facing = UP
-                        self.pikachu.y -= 1
+                        self.pika.facing = UP
+                        self.pika.y -= 1
                         break
 
-            if self.pikachu.is_trapped():
+            if self.pika.is_trapped():
                 break
-            self.rows[self.pikachu.y][self.pikachu.x] += 1
+            self.rows[self.pika.y][self.pika.x] += 1
 
-    def is_pikachu_back_at_the_start(self):
-        x, y = self.pikachu.x, self.pikachu.y
-        return (x, y) == self.pikachu.start and self.rows[y][x] != 0
+    def is_pikachu_back_at_the_start(self): 
+        return all([
+            (self.pika.x, self.pika.y) == self.pika.start,
+            self.rows[self.pika.y][self.pika.x] != 0
+        ])
 
     def pikachu_can_go_down(self):
-        x, y = self.pikachu.x, self.pikachu.y
-        return y < self.height - 1 and self.rows[y + 1][x] != WALL
+        return self.pika.y < self.height - 1 and self.rows[self.pika.y + 1][self.pika.x] != WALL
 
     def pikachu_can_go_left(self):
-        x, y = self.pikachu.x, self.pikachu.y
-        return x > 0 and self.rows[y][x - 1] != WALL
+        return self.pika.x > 0 and self.rows[self.pika.y][self.pika.x - 1] != WALL
 
     def pikachu_can_go_right(self):
-        x, y = self.pikachu.x, self.pikachu.y
-        return x < self.width - 1 and self.rows[y][x + 1] != WALL
+        return self.pika.x < self.width - 1 and self.rows[self.pika.y][self.pika.x + 1] != WALL
 
     def pikachu_can_go_up(self):
-        x, y = self.pikachu.x, self.pikachu.y
-        return y > 0 and self.rows[y - 1][x] != WALL
+        return self.pika.y > 0 and self.rows[self.pika.y - 1][self.pika.x] != WALL
 
     def print_solution(self):
         for y in range(self.height):
